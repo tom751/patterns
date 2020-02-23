0775c2be6e6678398704d15f5a37ef724a1464be
Tom Hilton <tomhilton93@gmail.com>
2020-02-23 11:58:34 +0000

    show list of pages on index

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,34 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
-import Image from '../components/image';
 import SEO from '../components/seo';
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    {data.allMarkdownRemark.edges.map(({ node }) => (
+      <div key={node.id}>
+        <Link to={node.frontmatter.path}>
+          <h3>{node.frontmatter.title}</h3>
+        </Link>
+      </div>
+    ))}
   </Layout>
 );
 
 export default IndexPage;
+
+export const query = graphql`
+  {
+    allMarkdownRemark(sort: { fields: frontmatter___title }) {
+      edges {
+        node {
+          frontmatter {
+            path
+            title
+          }
+        }
+      }
+    }
+  }
+`;
